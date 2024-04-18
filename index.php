5596babd0988db9b72ca55318ba5270ee7b209c2
tsuki_kami_ <tsuki_kami.private@proton.me>
2024-04-18 19:16:42 +0200

    gitignore

--- index.php
+++ index.php
@@ -70,7 +70,6 @@
         <button id="issue-copy"><i class="fas fa-copy"></i></button>
         <button id="issue-close"><i class="fas fa-times-circle"></i></button>
     </div>
-    <!-- Required libraries -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/components/prism-core.min.js"></script>
